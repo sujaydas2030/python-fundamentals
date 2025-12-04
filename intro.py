a519facd5592b98e32030ab1f1eb99d57c13a5b0
Sujay Das <sujaydas2030@gmail.com>
2025-12-04 16:10:08 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,8 @@
+# My GitHub Python Journey
+
+name = "Sujay Das"
+goal = "Become a Data Analyst / Data Scientist"
+
+print("Hello, my name is", name)
+print("Career Goal:", goal)
+print("This is my first GitHub program!")
